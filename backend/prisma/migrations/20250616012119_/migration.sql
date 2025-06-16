5b284f6ed2d2d94f04a187a9a803c9a6e3370321
Mesheal D. Somah <dsomah@gmail.com>
2025-06-16 01:31:49 +0000

    Updates

---
+++ backend/prisma/migrations/20250616012119_/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "applicantFather" ALTER COLUMN "fatherTelephoneNumber" SET DATA TYPE VARCHAR(14);
+
+-- AlterTable
+ALTER TABLE "applicantMother" ALTER COLUMN "motherTelephoneNumber" SET DATA TYPE VARCHAR(14);
